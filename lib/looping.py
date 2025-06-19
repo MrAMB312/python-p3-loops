05b7eb3491884fae96206ef3423c4c82435fccfa
Alex Blomarz <a.blomarz@gmail.com>
2025-06-18 20:31:38 -0500

    complete python-p3-loops lesson

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,28 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(f"{i}")
+        i -= 1
+        if i == 0:
+            print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_int_list = [integer * integer for integer in int_list]
+    return squared_int_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    i = 1
+    while i <= 100:
+        if (i % 3 == 0) and (i % 5 == 0):
+            print ("FizzBuzz")
+        elif i % 3 == 0:
+            print ("Fizz")
+        elif i % 5 == 0:
+            print ("Buzz")
+        else:
+            print (f"{i}")
+        i += 1
